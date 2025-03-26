8f93f6f257db9a8ab0365ec1e5922c45d923dc12
Silvia <silvia8gasparini@gmail.com>
2025-03-26 14:24:53 +0100

    right sidebar updates

--- script/homepage.js
+++ script/homepage.js
@@ -62,3 +62,25 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("Il contenitore per le card non è stato trovato.")
   }
 })
+
+const leftSidebar = document.getElementById("leftSidebar");
+const toggleLeft = document.getElementById("toggleLeft");
+
+toggleLeft.addEventListener("click", () => {
+  leftSidebar.classList.toggle("left-collapsed");
+});
+
+const toggleRightText = document.getElementById("toggleRightText");
+const rightSidebar = document.getElementById("rightSidebar");
+const closeRight = document.getElementById("closeRight");
+
+closeRight.addEventListener("click", () => {
+  rightSidebar.classList.add("hidden");
+  toggleRightText.classList.remove("d-none");
+});
+
+// Quando clicchi su "Attività amici" -> sidebar riappare, scritta scompare
+toggleRightText.addEventListener("click", () => {
+  rightSidebar.classList.remove("hidden");
+  toggleRightText.classList.add("d-none");
+});
